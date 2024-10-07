226f8b16d7fac101aab6965da107625985fe3383
George Pavlos <120208246+geo14150@users.noreply.github.com>
2024-10-07 18:45:59 +0300

    Add files via upload
    Track your expenses and reach your financial goals with react-investment-calculator - a React-based solution for managing personal finances.

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {useState} from 'react';
+
+import Header from './components/Header.jsx';
+import UserInput from './components/UserInput.jsx';
+import Results from './components/Results.jsx';
+
+function App() {
+  const [userInput,setUserInput]=useState({
+    initialInvestment :10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+});
+
+const inputIsValid = userInput.duration >=1;
+
+function handleChange (inputIdentifier, newValue){
+    setUserInput(prevState => {
+        return {
+            ...prevState,[inputIdentifier]: +newValue 
+        };C
+    });
+}
+
+
+
+
+
+  return ( <>
+
+    <Header/>
+    <UserInput userInput={userInput}   onChange={handleChange}/>
+    {!inputIsValid && <p className='center' >Please enter a duration  greater  than zero.</p>}
+    {inputIsValid && <Results input={userInput} />}
+    </>
+  );
+}
+
+export default App
